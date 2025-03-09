3ba193e8d1dd75adeac8a50590a99236c5c0f7dd
WacArteec <art.kaznad@gmail.com>
2025-03-09 22:47:50 +0300

    With different images

--- Crack/Crack.cpp
+++ Crack/Crack.cpp
@@ -3,11 +3,14 @@
 #include <assert.h>
 #include <TXLib.h>
 
-void Graphics()
+void Graphics(char* filename)
 {
     int height = 500, widht = 500;
-    double x0 = 100, x1 = 400, x2 = x0, y0 = 400, y1 = 450;
+    double x0 = 100, x1 = 400, x2 = 100, y0 = 400, y1 = 450;
+    double fl_x0 = 100, fl_x1 = 400, fl_y0 = 0, fl_y1 = 50;
     double delay = 100;
+
+    const char* images[4] = {"HackImages/Smile1.bmp", "HackImages/Smile2.bmp", "HackImages/Smile3.bmp", "HackImages/Smile4.bmp"}; 
     char crack_text[26] = "FILE'S BEING CRACKED:000%";
 
     txPlaySound("HACKGUITAR.wav");
@@ -18,12 +21,11 @@ void Graphics()
     txSetFillColor (TX_WHITE);
     txClear();
 
-    HDC image = txLoadImage("HackSmile.bmp", height, widht);
-    //printf("%p", image);
-    txBitBlt(txDC(), 0, 0, 0, 0, image);
-
     for(int i = 0; i < 101; i++)
     {
+        HDC image = txLoadImage(images[i / 26], height, widht);
+        txBitBlt(txDC(), 0, 0, 0, 0, image);
+
         txSetFillColor (TX_BLUE);
         txRectangle	(x0, y0, x1, y1);
 
@@ -40,6 +42,12 @@ void Graphics()
         txDrawText(x0, y0, x1, y1, crack_text);
 
         x2 += (x1 - x0) / 100;
+        
+        txSetFillColor (TX_BLUE);
+        txRectangle	(fl_x0, fl_y0, fl_x1, fl_y1);
+        
+        txSetTextAlign(TA_CENTER);
+        txDrawText(fl_x0, fl_y0, fl_x1, fl_y1, filename);
 
         txSleep(delay);
     }
@@ -47,13 +55,19 @@ void Graphics()
     txSetFillColor (TX_GREEN);
     txRectangle	(x0, y0, x2, y1);
     
+    txSetFillColor (TX_GREEN);
+    txRectangle	(fl_x0, fl_y0, fl_x1, fl_y1);
+    
+    txSetTextAlign(TA_CENTER);
+    txDrawText(fl_x0, fl_y0, fl_x1, fl_y1, filename);
+    
     txSetTextAlign(TA_CENTER);
     txDrawText(x0, y0 , x1, y1 , "FILE WAS CRACKED");
 
     txPlaySound(NULL);
 }
 
-void NewCOM(const char* filename, char old_symb, char new_symb)
+void NewCOM(char* filename, char old_symb, char new_symb)
 {
     FILE *bin_file = fopen(filename, "r+b");
     if (!bin_file) 
@@ -87,11 +101,11 @@ void NewCOM(const char* filename, char old_symb, char new_symb)
 
 int main()
 {
-    const char filename[] = "../PASART.COM";
+    char filename[] = "../PASART.COM";
 
     NewCOM(filename, 0x75, 0x74);
 
-    Graphics();
+    Graphics(filename);
 
     return 0;
 }
